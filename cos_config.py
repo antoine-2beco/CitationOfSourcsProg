1c39b7aa1867f9e8de5e8f5b1dd4302b56e944a1
Antoine <antoinedebeco001@gmail.com>
2022-01-28 19:25:53 +0100

    Commit V1

--- cos_config.py
+++ cos_config.py
@@ -1,4 +1,88 @@
 # coding: utf-8
 
-citation_config = {"Website Citation": f"",
-                   "Book Citation": }
+
+citation_config = {
+    "Website Citation": {
+        "Author": {
+            "obligatory": False,
+            "uppercase": True,
+            "lowercase": False,
+            "surrounded": None,
+        },
+        "Title of the page viewed": {
+            "obligatory": True,
+            "uppercase": False,
+            "lowercase": False,
+            "surrounded": ('«', '»'),
+        },
+        "Name of the site": {
+            "obligatory": True,
+            "uppercase": False,
+            "lowercase": False,
+            "surrounded": ('', '[en ligne]'),
+        },
+        "Publication date": {
+            "obligatory": False,
+            "uppercase": False,
+            "lowercase": False,
+            "surrounded": None,
+        },
+        "Link of the page": {
+            "obligatory": True,
+            "uppercase": False,
+            "lowercase": True,
+            "surrounded": ('sur', ''),
+        },
+        "Date of the viewed": {
+            "obligatory": True,
+            "uppercase": False,
+            "lowercase": False,
+            "surrounded": ('consultée le ', ''),
+        },
+
+    },
+    "Book Citation": {
+        "Author": {
+            "obligatory": True,
+            "uppercase": True,
+            "lowercase": False,
+            "surrounded": None,
+        },
+        "Title of the book": {
+            "obligatory": True,
+            "uppercase": False,
+            "lowercase": False,
+            "surrounded": ('«', '»'),
+        },
+        "Place of edition": {
+            "obligatory": True,
+            "uppercase": False,
+            "lowercase": False,
+            "surrounded": None,
+        },
+        "Name of the editor": {
+            "obligatory": True,
+            "uppercase": False,
+            "lowercase": False,
+            "surrounded": None,
+        },
+        "Date of publication": {
+            "obligatory": True,
+            "uppercase": False,
+            "lowercase": False,
+            "surrounded": None,
+        },
+        "Collection": {
+            "obligatory": False,
+            "uppercase": False,
+            "lowercase": False,
+            "surrounded": None,
+        },
+        "Reproduced pages": {
+            "obligatory": False,
+            "uppercase": False,
+            "lowercase": False,
+            "surrounded": None,
+        },
+    },
+}
--- cos_main.py
+++ cos_main.py
@@ -1,5 +1,45 @@
 # coding: utf-8
+import cos_config
 
 
-def show_menu():
-    for
+def main_menu():
+    for index, citation_type in enumerate(cos_config.citation_config.keys()):
+        print(f"{index+1}. {citation_type}")
+
+    choice = input("Make a choice : ")
+    if not choice.isdigit() or not (0 < int(choice) < (index + 2)):
+        print("Error, relaunch the programm.")
+        return
+
+    return list(cos_config.citation_config)[int(choice)-1]
+
+
+def get_elements(choice):
+    print("Please give the following elements.")
+    elements = []
+    for index, element in enumerate(cos_config.citation_config[choice]):
+        input_element = input(f"{index + 1}. {element} : ")
+
+
+        if cos_config.citation_config[choice][element]['obligatory'] and input_element is None:
+            print("It is obligatory to enter this element.")
+            while input_element is not None :
+                input_element = input(f"Retry :: {index}. {choice} : ")
+
+        if cos_config.citation_config[choice][element]['uppercase']:
+            input_element = input_element.upper()
+
+        if cos_config.citation_config[choice][element]['lowercase']:
+            input_element = input_element.lower()
+
+        if cos_config.citation_config[choice][element]['surrounded']:
+            input_element = cos_config.citation_config[choice][element]['surrounded'][0] + input_element + cos_config.citation_config[choice][element]['surrounded'][1]
+
+
+        elements.append(input_element)
+
+    print(', '.join(elements) + '.')
+
+
+
+get_elements(main_menu())
